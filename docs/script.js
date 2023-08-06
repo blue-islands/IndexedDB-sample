ed8939cccb723b8881b4091e34ec33a67214da7a
HIDEKAZU AOSHIMA <h.aoshima@livlog.jp>
2023-08-06 18:57:49 +0900

    削除処理追加

--- docs/script.js
+++ docs/script.js
@@ -1,27 +1,22 @@
 const PageState = {
   db: null,
-
   init() {
     const request = indexedDB.open("myDatabase", 1);
-
     request.onupgradeneeded = (event) => {
       this.db = event.target.result;
       const objectStore = this.db.createObjectStore("pageState", {
         keyPath: "url",
       });
     };
-
     request.onsuccess = (event) => {
       this.db = event.target.result;
     };
   },
-
   saveState(data) {
     const transaction = this.db.transaction(["pageState"], "readwrite");
     const objectStore = transaction.objectStore("pageState");
     objectStore.put({ url: window.location.href, data });
   },
-
   loadState() {
     const transaction = this.db.transaction(["pageState"]);
     const objectStore = transaction.objectStore("pageState");
@@ -32,6 +27,19 @@ const PageState = {
       };
       request.onerror = (event) => {
         reject(new Error("Data retrieval failed"));
+      };
+    });
+  },
+  deleteState() {
+    const transaction = this.db.transaction(["pageState"], "readwrite");
+    const objectStore = transaction.objectStore("pageState");
+    const request = objectStore.delete(window.location.href);
+    return new Promise((resolve, reject) => {
+      request.onsuccess = (event) => {
+        resolve(true);
+      };
+      request.onerror = (event) => {
+        reject(new Error("Data deletion failed"));
       };
     });
   },
