e43f763e97779cbb08b37904a73e062d6a282bd3
HIDEKAZU AOSHIMA <h.aoshima@livlog.jp>
2023-08-06 16:52:58 +0900

    修正

--- docs/script.js
+++ docs/script.js
@@ -1,36 +1,39 @@
-var db;
+var PageState = {
+  db: null,
 
-// データベースを開く
-var request = indexedDB.open("myDatabase", 1);
+  init: function () {
+    var request = indexedDB.open("myDatabase", 1);
 
-request.onupgradeneeded = function (event) {
-  db = event.target.result;
-  // keyPathを'url'に変更
-  var objectStore = db.createObjectStore("pageState", { keyPath: "url" });
+    request.onupgradeneeded = function (event) {
+      PageState.db = event.target.result;
+      var objectStore = PageState.db.createObjectStore("pageState", {
+        keyPath: "url",
+      });
+    };
+
+    request.onsuccess = function (event) {
+      PageState.db = event.target.result;
+    };
+  },
+
+  saveState: function () {
+    var data = document.getElementById("dataInput").value;
+    var transaction = PageState.db.transaction(["pageState"], "readwrite");
+    var objectStore = transaction.objectStore("pageState");
+    objectStore.put({ url: window.location.href, data: data });
+  },
+
+  loadState: function () {
+    var transaction = PageState.db.transaction(["pageState"]);
+    var objectStore = transaction.objectStore("pageState");
+    var request = objectStore.get(window.location.href);
+    request.onsuccess = function (event) {
+      document.getElementById("output").innerText = request.result
+        ? request.result.data
+        : "No data found";
+    };
+  },
 };
 
-request.onsuccess = function (event) {
-  db = event.target.result;
-};
-
-// データを保存
-function saveState() {
-  var data = document.getElementById("dataInput").value;
-  var transaction = db.transaction(["pageState"], "readwrite");
-  var objectStore = transaction.objectStore("pageState");
-  // idの代わりにurlをキーとして使用
-  objectStore.put({ url: window.location.href, data: data });
-}
-
-// データを取得
-function loadState() {
-  var transaction = db.transaction(["pageState"]);
-  var objectStore = transaction.objectStore("pageState");
-  // 現在のページのURLをキーとしてデータを取得
-  var request = objectStore.get(window.location.href);
-  request.onsuccess = function (event) {
-    document.getElementById("output").innerText = request.result
-      ? request.result.data
-      : "No data found";
-  };
-}
+// 初期化
+PageState.init();
