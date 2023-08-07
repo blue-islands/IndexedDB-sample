3e81e7f936f110b680226a790795ff893df003c4
HIDEKAZU AOSHIMA <h.aoshima@livlog.jp>
2023-08-08 08:43:21 +0900

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -15,20 +15,28 @@ const PageState = {
   saveState(data) {
     const transaction = this.db.transaction(["pageState"], "readwrite");
     const objectStore = transaction.objectStore("pageState");
-    objectStore.put({ url: window.location.href, data });
+    const now = new Date().getTime(); // 現在の日時を取得
+    objectStore.put({ url: window.location.href, data, timestamp: now });
   },
   loadState() {
-    const transaction = this.db.transaction(["pageState"]);
-    const objectStore = transaction.objectStore("pageState");
-    const request = objectStore.get(window.location.href);
-    return new Promise((resolve, reject) => {
-      request.onsuccess = (event) => {
-        resolve(request.result ? request.result.data : null);
-      };
-      request.onerror = (event) => {
-        reject(new Error("Data retrieval failed"));
-      };
-    });
+   const transaction = this.db.transaction(["pageState"]);
+   const objectStore = transaction.objectStore("pageState");
+   const request = objectStore.get(window.location.href);
+   return new Promise((resolve, reject) => {
+     request.onsuccess = (event) => {
+       const now = new Date().getTime();
+       const oneDay = 24 * 60 * 60 * 1000; // 24時間をミリ秒で表現
+       if (request.result && now - request.result.timestamp <= oneDay) {
+         resolve(request.result.data);
+       } else {
+         this.deleteState(); // 期間を超えているデータを削除
+         resolve(null);
+       }
+     };
+     request.onerror = (event) => {
+       reject(new Error("Data retrieval failed"));
+     };
+   });
   },
   deleteState() {
     const transaction = this.db.transaction(["pageState"], "readwrite");
